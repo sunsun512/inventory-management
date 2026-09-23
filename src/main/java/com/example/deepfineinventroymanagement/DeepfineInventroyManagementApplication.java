5208c9fdac80acbd559593b51cc886ebd0faba18
sunsun512 <hyeseon@hyeseon-ui-MacBookAir.local>
2026-09-23 21:22:37 +0900

    init

---
+++ src/main/java/com/example/deepfineinventroymanagement/DeepfineInventroyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.deepfineinventroymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepfineInventroyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepfineInventroyManagementApplication.class, args);
+    }
+
+}
