1c38112764cfcf42bc14344c49a6bc1436ffc99a
sunsun512 <100753335+sunsun512@users.noreply.github.com>
2026-09-23 21:22:37 +0900

    init

---
+++ src/test/java/com/example/deepfineinventroymanagement/DeepfineInventroyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deepfineinventroymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepfineInventroyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
